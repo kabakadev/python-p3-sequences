c462075d7f007d08622ab43c893b34a07c886a8d
Ian Kabaka <iankabaka1@gmail.com>
2024-11-23 13:36:43 +0300

    working fibonnacci for p3-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    fibonacci = [0, 1]
+    if length == 0:
+        print( [])
+    
+    elif length == 1:
+        print( [0])
+        
+    elif length == 2:
+        print( [0,1])
+    else:
+        while len(fibonacci) <length:
+            newnum = fibonacci[-1] + fibonacci[-2]
+            fibonacci.append(newnum)
+        print(fibonacci)
+        
+
+    
+
+   
+    
+    
